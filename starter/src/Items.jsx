b31600f067ba3594c7ad0d30fdf75976b6884b57
Praveen Kumar <praveenkumarthipparam@gmail.com>
2023-09-10 23:54:21 -0500

    First Query

--- starter/src/Items.jsx
+++ starter/src/Items.jsx
@@ -1,7 +1,18 @@
+import { useQuery } from '@tanstack/react-query';
+import customFetch from './utils';
+
 import SingleItem from './SingleItem';
+
 const Items = ({ items }) => {
+  const result = useQuery({
+    queryKey: ['tasks'],
+    queryFn: () => customFetch.get('/'),
+  });
+
+  console.log(result);
+
   return (
-    <div className='items'>
+    <div className="items">
       {items.map((item) => {
         return <SingleItem key={item.id} item={item} />;
       })}
